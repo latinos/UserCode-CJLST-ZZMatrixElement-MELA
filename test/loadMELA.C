582cc55748130aceb101bee9c89c6374fb787d1c
bonato <>
2012-10-11 11:33:32 +0000

    Add class for producing SuperMELA (only mH=125 and 126 GeV, for the moment)

--- test/loadMELA.C
+++ test/loadMELA.C
@@ -4,5 +4,7 @@
 {
   gSystem->Load("libZZMatrixElementMELA.so");
   gROOT->LoadMacro("$CMSSW_BASE/src/ZZMatrixElement/MELA/interface/Mela.h+");
+ gROOT->LoadMacro("$CMSSW_BASE/src/ZZMatrixElement/MELA/interface/SuperMELA.h+");
+ gROOT->LoadMacro("$CMSSW_BASE/src/ZZMatrixElement/MELA/interface/PseudoMELA.h+");
   gSystem->AddIncludePath("-I$CMSSW_BASE/src/ ");  
 }
