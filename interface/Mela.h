13a86475dd1d6e2ef2f8414993bc93f009f4f966
mmozer <>
2012-11-21 12:43:41 +0000

    add MCFM/JHUGen Matrix element computation

--- interface/Mela.h
+++ interface/Mela.h
@@ -6,8 +6,8 @@
  *  MELA discriminator
  *
  *
- *  $Date: 2012/09/26 20:14:43 $
- *  $Revision: 1.11 $
+ *  $Date: 2012/10/02 12:50:39 $
+ *  $Revision: 1.12 $
  *  \author JHU
  */
 
@@ -22,6 +22,8 @@ class RooRealVar;
 class RooAbsPdf;
 class RooArgSet;
 class AngularPdfFactory;
+class TensorPdfFactory;
+class ZZMatrixElement;
 
 
 class Mela { 
@@ -53,7 +55,38 @@ public:
 		 bool withY = false);
 
 
+  void computeP(float mZZ, float mZ1, float mZ2, 
+		float costhetastar,
+		float costheta1, 
+		float costheta2,
+		float phi,
+		float phistar1,
+		//signal probabilities
+		float& p0plus_melaNorm,   // higgs, analytic distribution, normalized as for normal MELA distribution     
+		float& p0plus_mela,   // higgs, analytic distribution          
+		float& p0minus_mela,  // pseudoscalar, analytic distribution 
+		float& p0plus_VAJHU,  // higgs, vector algebra, JHUgen
+		float& p0minus_VAJHU, // pseudoscalar, vector algebra, JHUgen
+		float& p0plus_VAMCFM,// higgs, vector algebra, MCFM
+		float& p1_mela,  // zprime, analytic distribution 
+		float& p1_VAJHU, // zprime, vector algebra, JHUgen,
+		float& p2_mela , // graviton, analytic distribution 
+		float& p2_VAJHU, // graviton, vector algebra, JHUgen,
+		//backgrounds
+		float& bkg_mela,  // background,  analytic distribution 
+		float& bkg_VAMCFM, // background, vector algebra, MCFM
+		//pt/rapidity
+		float& p0_pt, // multiplicative probability for signal pt
+		float& p0_y, // multiplicative probability for signal y
+		float& bkg_pt, // multiplicative probability for bkg pt
+		float& bkg_y, // multiplicative probability for bkg y
+		//optional input parameters
+		float pt4l=0.0,
+		float Y4l=0.0,
+		int flavor=1 // 1:4e, 2:4mu, 3:2e2mu (for interference effects)
+		);
 
+  
   /// Compute KD from masses and angles. 
   /// The user must ensure that the order of m1/m2 matches the order of theta1/theta2.
   void computeKD(float mZZ, float mZ1, float mZ2, 
@@ -120,8 +153,12 @@ private:
   RooRealVar* upFrac_rrv;
   
   AngularPdfFactory* SMHiggs;
+  AngularPdfFactory* PSHiggs;
+  TensorPdfFactory *minGrav;   
   RooAbsPdf* SMZgammaZZ;
   RooAbsPdf* SMZZ;
+  
+  ZZMatrixElement* ZZME;
 
   std::vector<RooRealVar*> ptparamsS;
   std::vector<RooRealVar*> ptparamsB;
---
+++ interface/TModHiggsMatEl.hh
@@ -0,0 +1,10 @@
+
+#ifndef _TMODHIGGSMATEL_HH_
+#define _TMODHIGGSMATEL_HH_
+
+extern "C" {
+  void __modhiggs_MOD_evalamp_gg_h_vv(double P[6][4], double *MReso,  double *GaReso, double *Hggcoupl, double *Hvvcoupl, int *MYIDUP, double *MatElSq);
+}
+
+#endif
+
